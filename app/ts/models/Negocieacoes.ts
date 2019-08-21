ae125dacfd6f63f32816d1c44fd10c0e864c77d4
Breno Cupertino <breno_cupertino@live.com>
2019-08-21 02:21:25 -0300

    new skill

---
+++ app/ts/models/Negocieacoes.ts
@@ -0,0 +1,27 @@
+import { MeuObjeto } from './MeuObjeto';
+import { Negociacao } from './Negociacao';
+
+export class Negociacoes implements MeuObjeto<Negociacoes> {
+    
+    ehIgual(negociacoes: Negociacoes): boolean {
+        
+        return JSON.stringify(this._negociacoes) == JSON.stringify(negociacoes.paraArray);
+    }
+
+    private _negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao): void {
+
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(): Negociacao[] {
+        
+        return ([] as Negociacao[]).concat(this._negociacoes);
+    }
+
+    paraTexto(): void {
+
+        console.log(JSON.stringify(this._negociacoes));
+    }
+}
---
+++ app/ts/models/index.ts
@@ -0,0 +1,6 @@
+export * from './Negociacao';
+export * from './Negocieacoes';
+export * from './NegociacaoParcial';
+export * from './Imprimivel';
+export * from './Igualavel';
+export * from './MeuObjeto';
